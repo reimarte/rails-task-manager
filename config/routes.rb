2f9dfba94fc3ade37907b7e74152e07de623b0ed
laura reimarte <reimarte.laura@gmail.com>
2022-07-16 17:27:29 +0100

    complete CRU of CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "index", to: "tasks#index"
+
+  get "new", to: "tasks#new", as: :new
+  post "tasks", to: "tasks#create"
+
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch "tasks/:id", to: "tasks#update"
 end
- 
